0e1940b3c8769234791fdca5807f15654a6e6b56
tzony4 <tonijimenezlamas@gmail.com>
2025-04-03 09:39:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -50,4 +50,5 @@ def llista_llibres():
     return jsonify(resultat)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
